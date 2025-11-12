f587845aede66833910588a3526801a4e4e68337
AKI YU <nobodyyu@gmail.com>
2025-11-12 09:02:50 +0800

    update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "fb4beb705c0a2016899d25353753cfff",
+const RESOURCES = {"flutter_bootstrap.js": "ae1feeb2520d044860ab7c8844034815",
 "version.json": "b50c2ea3b0a512bca0cd1538c569bdcd",
 "index.html": "4294c717eeb0b73397bb7db2452d1d29",
 "/": "4294c717eeb0b73397bb7db2452d1d29",
-"main.dart.js": "c5004f5407624942718f3969b79816f0",
+"main.dart.js": "c62c7159a8ed40244c4b1882bf3498a8",
 "404.html": "4294c717eeb0b73397bb7db2452d1d29",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "da2dd5ba3373bd53f49798e24bc558c7",
