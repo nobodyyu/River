b01deff43d912a08b25179cc8baa0d8c6ac28072
AKI YU <nobodyyu@gmail.com>
2025-11-24 23:32:45 +0800

    update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"70a3c3cc935e49b44db818348dd2d57f055ab7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3046827562"
+    serviceWorkerVersion: "1152725171"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,12 +3,13 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "bb1a8a9fe57a00fddb7947e54ebc4006",
+const RESOURCES = {"flutter_bootstrap.js": "05f69cb0e7241a17fe606c53e0b28463",
 "version.json": "b50c2ea3b0a512bca0cd1538c569bdcd",
 "index.html": "4294c717eeb0b73397bb7db2452d1d29",
 "/": "4294c717eeb0b73397bb7db2452d1d29",
 "CNAME": "0f39dfc949da8248a5b3c1faf6916f86",
-"main.dart.js": "49a40dc8998f21d75420a5cbc0fa84db",
+"main.dart.js": "c8d2ffa7745dc80b585eaf313cfa865e",
+"404.html": "4294c717eeb0b73397bb7db2452d1d29",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "da2dd5ba3373bd53f49798e24bc558c7",
 "icons/river.png": "001797fa449d888c697bc3d2ef7d23c8",
